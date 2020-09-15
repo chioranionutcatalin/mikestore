2771bc660669fda0f566500383b11dedc53fb555
chioranionutcatalin <ionut.chioran@svrolp02131.station>
2020-09-15 22:45:48 +0200

    fix table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,83 @@
 import React from 'react';
-import {Table } from './Table'
+import {Table} from './Table';
+import config from './config'
 
-function App() {
-  return (
-    <div>
-      <Table clients={[]}/>
+export class App extends React.Component {
+  state = {
+    clients: [],
+    error: null
+  }
+  
+  componentDidMount() {
+    // 1. Load the JavaScript client library.
+    window.gapi.load("client", this.initClient);
+  }
+
+  load(callback) {
+    window.gapi.client.load("sheets", "v4", () => {
+      window.gapi.client.sheets.spreadsheets.values
+        .get({
+          spreadsheetId: config.spreadsheetId,
+          range: "Sheet1!A2:T"
+        })
+        .then(
+          response => {
+            const data = response.result.values;
+            const dataFiltered = data.filter((dat)=> dat.length !== 1);
+            const clients = dataFiltered.map(clientProps => ({
+              nome: clientProps[1],
+              contatto: clientProps[2],
+              codiceArticolo: clientProps[3],
+              altro: clientProps[4],
+              col: clientProps[0],
+            })) || [];
+            callback({
+              clients
+            });
+          },
+          response => {
+            callback(false, response.result.error);
+          }
+        );
+    });
+  }
+
+  initClient = () => {
+    // 2. Initialize the JavaScript client library.
+    window.gapi.client
+      .init({
+        apiKey: config.apiKey,
+        // Your API key will be automatically added to the Discovery Document URLs.
+        discoveryDocs: config.discoveryDocs
+      })
+      .then(() => {
+      // 3. Initialize and make the API request.
+      this.load(this.onLoad);
+    });
+  };
+  
+  onLoad = (data, error) => {
+    if (data) {
+      const clients = data.clients;
+      this.setState({ clients });
+    } else {
+      this.setState({ clients: [] , error});
+    }
+  };
+
+  render() {
+  
+    const { clients, error } = this.state;
+    const staCaricando = clients.length === 0;
+    const isError = error !== null;
+    
+    return (
+    <div className="App">
+      {isError && <h1>Riprova caricare la pagina c'e' qualcosa che non va!</h1>}
+      {staCaricando ? <h1>Stiamo caricando I dati</h1> : <Table clients={clients}/>}
     </div>
   );
 }
+};
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,62 +1,45 @@
 import React from 'react';
-import MaterialTable from 'material-table';
+import ReactTable from 'react-table-v6'
+import 'react-table-v6/react-table.css'
 
  export const Table = ({clients}) => {
-  console.log(clients);  
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'nome', field: 'nome'},
-      { title: 'Articolo Comprato', field: 'codiceArticolo' },
-      { title: 'Contatto', field: 'contatto'},
-      { title: 'Altro info', field: 'altro'},
-    ],
-    data: [{nome: 'gigi', articoloComprato: '241241241', contatto: 'afasafa', alto: 'fafsa'}],
-  });
+
+    const columns = [{
+      Header: 'Mike clients',
+      columns : [
+        {
+          Header: 'Nome',
+          accessor: 'nome',
+          maxWidth: 200,
+        }, {
+          Header: 'Contatto',
+          accessor: 'contatto',
+          maxWidth: 200,
+        }, {
+          Header: 'codiceArticolo',
+          accessor: 'codiceArticolo',
+          maxWidth: 600,
+        }, {
+          Header: 'altro Info',
+          accessor: 'altro',
+          maxWidth: 600,
+        }
+      ] 
+    }];
+
   
   return (
-    <MaterialTable
-      title="Clienti Mike srl"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
+    <ReactTable
+      data={clients}
+      filterable
+          defaultFilterMethod={(filter, row) => {
+            return String(row[filter.id]).toLowerCase().includes(filter.value.toLowerCase())
+          }
+            
+          }
+      columns={columns}
+      defaultPageSize={10}
+      className="-striped -highlight"
     />
   );
-}
+        };
