bdc37992dc0d11df461b6425e0fbd3a241205bc1
chioranionutcatalin <ionut.chioran@svrolp02131.station>
2020-09-18 20:38:01 +0200

    fix stuct2

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ const path = require('path');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
-// serve up production assets
-app.use(express.static('client/build'));
-// let the react app to handle any unknown routes 
-// serve up the index.html if express does'nt recognize the route
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use('/api', require('./router/sheet'));
 
